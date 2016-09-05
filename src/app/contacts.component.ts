d62a2b1c97d5406845e7d2aba7367ac442f2f9d2
Fabian Keller <fabian.keller@mimacom.com>
2016-09-05 10:58:55 +0200

    Update: display list of contacts

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -9,19 +10,5 @@ import { Contact } from './models/contact';
 export class ContactsAppComponent {
   title = 'Angular 2 Master Class setup works!';
 
-  private contact: Contact = {
-    id: 7,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/7.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  };
+  private contacts: Contact[] = CONTACT_DATA;
 }
